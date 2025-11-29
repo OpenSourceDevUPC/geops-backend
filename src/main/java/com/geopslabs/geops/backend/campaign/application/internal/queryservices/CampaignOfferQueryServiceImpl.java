00a85157d210e04cac6950b52f63baaa1325f35b
MaineMa <144504783+MaineMa@users.noreply.github.com>
2025-11-29 09:17:12 -0500

    feat: Added methods for Repositories. Added implementation for services.

---
+++ src/main/java/com/geopslabs/geops/backend/campaign/application/internal/queryservices/CampaignOfferQueryServiceImpl.java
@@ -0,0 +1,9 @@
+package com.geopslabs.geops.backend.campaign.application.internal.queryservices;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CampaignOfferQueryServiceImpl {
+}
